9560118c8244233d0adac05ea714770d56853179
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2025-11-17 06:02:31 +0800

    Update shadowsocks.js

--- shadowsocks.js
+++ shadowsocks.js
@@ -3,8 +3,8 @@
 import { connect } from 'cloudflare:sockets';
 
 let subPath = 'link';     // 节点订阅路径,不修改将使用UUID作为订阅路径
-let proxyIP = '13.230.34.30';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
-let password = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63';  // 节点UUID
+let proxyIP = '213.189.58.89:47281';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
+let password = '7dc16e15-f285-4a9d-959b-0e4fbdd77b63';  // 节点UUID
 let SSpath = '';          // 路径验证，如果为空则使用UUID作为验证路径
 
 // CF CDN 
