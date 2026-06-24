fb4ca8aa76f485a6b2ebdca9502772f6cde096d3
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2026-06-24 21:14:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ let subPath = 'link';     // 节点订阅路径,不修改将使用uuid作为订�
 let password = '123456';  // 主页密码,建议修改或添加 PASSWORD环境变量
 let proxyIP = 'proxy.xxxxxxxx.tk:50001';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
 let yourUUID = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63'; // UUID,建议修改或添加环境便量
-let disabletro = false;  // 是否关闭trojan, 设置为true时关闭，false开启 
+let disabletro = true;  // 是否关闭trojan, 设置为true时关闭，false开启 
 
 // CDN 
 let cfip = [ // 格式:优选域名:端口#备注名称、优选IP:端口#备注名称、[ipv6优选]:端口#备注名称、优选域名#备注 
